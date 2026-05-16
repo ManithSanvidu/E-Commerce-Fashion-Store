001e6cb458dd74b0e768cf37e1615f8e9989273f
IT24101458-M.G.M. Sanvidu <165678028+ManithSanvidu@users.noreply.github.com>
2026-05-16 11:56:29 +0530

    Add files via upload

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -1,13 +1,24 @@
 import Product from "../models/Product.js"
 
 export const getProducts=async(req,res)=>{
-    const products=await Product.find();
-    res.json(products);
+    try {
+        const products=await Product.find();
+        res.json(products);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
 export const getProductById=async(req,res)=>{
-    const product=await Product.findById(req.params.id);
-    res.json(product);
+    try {
+        const product=await Product.findById(req.params.id);
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+        res.json(product);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
 export const createProduct = async (req, res) => {
